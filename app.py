df5cef943c8d4ee473dbe054326e4526a056227e
Mohammad Mashukur Rahman <mr683386@gmail.com>
2026-04-08 17:07:45 +0100

    Add code to app.py + Dockerfile + requirements.txt

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return """
+	<h1>Hello from Kubernetes!</h1>
+	<p>Your Python Flask app is successfully running inside a Kubernetes Pod.</p>
+	<p>This page is being served by Flask on port 80</p>
+	"""
+if __name__ == '__main__':
+	print("Starting Flask web server on port 80 ...")
+	app.run(host='0.0.0.0', port=80, debug=False)
